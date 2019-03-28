a619a59c58b42e82c0e9b1e8cd04f4b38c303a28
betapixel <alejoarias@gmail.com>
2019-03-27 21:13:44 -0300

    Fin de la seccion 6

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,20 +8,25 @@ import { HttpClient } from '@angular/common/http';
 export class ProductosService {
 
   cargando = true;
+  productos:Producto[]=[];
 
   constructor(private http: HttpClient) { 
 
     this.cargarProductos();
     
   }
-      private cargarProductos() {
-        
-        this.http.get('https://angular-html-722d7.firebaseio.com/productos_idx.json');
-        .subscribe((resp: any[]) =>{
-          // this.productos = resp;
-          console.log(resp);
-          
+
+
+  private cargarProductos() {
+    
+    this.http.get('https://angular-html-722d7.firebaseio.com/productos_idx.json')
+        .subscribe((resp: Producto[]) => {
+
+        console.log(resp);
+        this.productos = resp;
+        this.cargando = false;
+      
         });
-        
-      }
+    
+  }
 }
